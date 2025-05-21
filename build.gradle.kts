d774360dae1313cc7642a2807e8dade25f922bca
Daniil Serov <alnmlbch@gmail.com>
2025-05-21 20:30:35 +0300

    build: update reactive dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,15 +19,17 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.r2dbc:r2dbc-h2")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 }
 
 kotlin {
--- src/main/kotlin/org/example/mdblog/MdBlogApplication.kt
+++ src/main/kotlin/org/example/mdblog/MdBlogApplication.kt
@@ -2,10 +2,10 @@ package org.example.mdblog
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 
 @SpringBootApplication
-@EnableJdbcRepositories
+@EnableR2dbcRepositories
 class MdBlogApplication
 
 fun main(args: Array<String>) {
