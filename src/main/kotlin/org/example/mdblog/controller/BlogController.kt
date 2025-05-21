acfbf308096126ec6efdb730470210e6a269a846
Daniil Serov <alnmlbch@gmail.com>
2025-05-21 16:46:14 +0300

    feat: add forgotten file

--- src/main/kotlin/org/example/mdblog/controller/BlogController.kt
+++ src/main/kotlin/org/example/mdblog/controller/BlogController.kt
@@ -2,5 +2,18 @@ package org.example.mdblog.controller
 
 import org.springframework.web.bind.annotation.RestController
 
+// localhost:8080/{login}/{title}/{postTitle}
+
+// localhost:8080/{login}/topics
+// localhost:8080/{login}/{title}/posts
+
+// localhost:8080/{login}/{title}?tag=tag
+
 @RestController
-class BlogController
+class AuthorController
+
+@RestController
+class TopicController
+
+@RestController
+class PostController
